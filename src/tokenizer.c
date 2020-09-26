21daf992e6eafe2451efb5f49b8cd0080a7fe3cb
student <daayala2@miners.utep.edu.com>
2020-09-26 16:08:33 -0600

    finished word_start untested

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -28,6 +28,17 @@ int non_space_char(char c);
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
 char word_start(char *str);
+{
+  while(*str != '\0')
+    {
+      if (non_space_char(*str) == 1)
+	return str;
+      else if (*str == '\0')
+	return str;
+      str++;
+    }
+  return str;
+}
 
 /* Returns a pointer terminator char following *word */
 char *word_terminator(char *word);
