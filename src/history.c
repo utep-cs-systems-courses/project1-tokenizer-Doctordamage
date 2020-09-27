0aa42567f6390a94401f8a6d52feb5627a037a0b
student <daayala2@miners.utep.edu.com>
2020-09-26 21:31:26 -0600

    finished history.c

--- src/history.c
+++ src/history.c
@@ -3,22 +3,6 @@
 #include "history.h"
 #include "tokenizer.h"
 
-typedef struct s_Item
-{
-  int id;
-  char *str;
-  struct s_Item *next;
-} Item;
-
-
-
-typedef struct s_List
-{
-  struct s_Item *root;
-} List;
-
-
-
 /* Initialize the linked list to keep the history. */
 List *init_history()
 {
@@ -27,28 +11,59 @@ List *init_history()
   return list;
 }
 
-
-
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
-
-
+void add_history(List *list, char *str)
+{
+  int index = 1;
+  Item *node = list->root;
+  while (node->next)
+    {
+      node = node->next;
+      index++;
+    }
+  node->next = malloc(sizeof(Item));
+  node->next->str = copy_str(str, string_length(str));
+  node->next->id = index;
+}
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id);
-
-
+char *get_history(List *list, int id)
+{
+  Item *node = list->root;
+  while (node->next)
+    {
+      if (node->id == id) 
+	return node->str;
+      node = node->next;
+    }
+}
 
 /*Print the entire contents of the list. */
-void print_history(List *list);
-
-
+void print_history(List *list)
+{
+  Item *node = list->root;
+  while (node)
+    {
+      printf("%d. %s", node->id, node->str);
+      node = node->next;
+    }
+}
 
 /*Free the history list and the strings it references. */
-void free_history(List *list);
+void free_history(List *list)
+{
+  Item *current = list->root;
+  while (current)
+    {
+      Item *next = current->next;
+      free(current);
+      current = next;
+    }
+  free(list);
+}
 
